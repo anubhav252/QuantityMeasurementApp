a04db923ee805db60393790789818678c34f325f
anubhav252 <yanubhav977@gmail.com>
2026-03-12 15:32:33 +0530

    [Anubhav Yadav] Add. created centralized arithmetic operarions logic

---
+++ QuantityMeasurementMain/Interfaces/IMeasurable.cs
@@ -0,0 +1,10 @@
+namespace QuantityMeasurementMain.Interfaces
+{
+    public interface IMeasurable
+    {
+        double GetConversionFactor();
+        double ConvertToBaseUnit(double value);
+        double ConvertFromBaseUnit(double baseValue);
+        string GetUnitName();
+    }
+}
